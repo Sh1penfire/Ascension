6c9b197ddabdb05b7ac8637c81b66a1004d99d2b
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2023-02-21 00:03:32 +1100

    Simple script

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function end(){
+  Vars.player.team().cores().each(c => c.kill());
+  Sounds.wind3.play();
+}
+
+Events.on(UnitDestroyEvent, e => {
+  if(e.unit.team == Vars.player.team()) end();
+});
+
+Events.on(BlockDestroyEvent, e => {
+  if(e.tile.build.team == Vars.player.team() && !(e.tile.build.block.class instanceof CoreBlock) && Vars.player.team().cores().size != 1) end();
+});
